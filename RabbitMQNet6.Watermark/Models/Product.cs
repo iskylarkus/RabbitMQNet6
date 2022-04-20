89c36d735befbc820794b186f3f9bd219791e7c8
iskylarkus <turgay.kibar@gmail.com>
2022-04-20 09:13:05 +0300

    product entity class created

---
+++ RabbitMQNet6.Watermark/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RabbitMQNet6.Watermark.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+
+        [Range(0, 900)]
+        public int Stock { get; set; }
+
+        [StringLength(500)]
+        public string PictureUrl { get; set; }
+    }
+}
