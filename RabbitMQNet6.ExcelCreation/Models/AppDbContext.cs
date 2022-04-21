81a69950a95957cdfc5d4d873641df4e3944fc18
iskylarkus <turgay.kibar@gmail.com>
2022-04-21 19:05:56 +0300

    app db context created

---
+++ RabbitMQNet6.ExcelCreation/Models/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace RabbitMQNet6.ExcelCreation.Models
+{
+    public class AppDbContext:IdentityDbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<UserFile> UserFiles { get; set; }
+    }
+}
