1b12d2d183486ba8f861cf945fbe63208eee51d4
iskylarkus <turgay.kibar@gmail.com>
2022-04-21 19:05:36 +0300

    user file entity created

---
+++ RabbitMQNet6.ExcelCreation/Models/UserFile.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RabbitMQNet6.ExcelCreation.Models
+{
+    public class UserFile
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public string FileName { get; set; }
+        public string FilePath { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public FileStatus FileStatus { get; set; }
+
+        [NotMapped]
+        public string GetCreatedDate => CreatedDate.HasValue ? CreatedDate.Value.ToShortDateString() : "-";
+    }
+
+    public enum FileStatus
+    {
+        Creating = 0,
+        Completed = 1
+    }
+}
