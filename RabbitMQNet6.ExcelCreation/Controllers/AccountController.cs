7f73c52adc3f89577c60e5dce991b9ecd7999339
iskylarkus <turgay.kibar@gmail.com>
2022-04-22 18:34:58 +0300

    account controller and view created

---
+++ RabbitMQNet6.ExcelCreation/Controllers/AccountController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RabbitMQNet6.ExcelCreation.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(string Email, string Password)
+        {
+            var hasUser = await _userManager.FindByEmailAsync(Email);
+
+            if (hasUser != null)
+            {
+                var signInResult = await _signInManager.PasswordSignInAsync(hasUser, Password, true, false);
+
+                if (signInResult.Succeeded)
+                {
+                    return RedirectToAction(nameof(HomeController.Index), "Home");
+                }
+            }
+
+            return View();
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+
+            return RedirectToAction(nameof(HomeController.Index), "Home");
+        }
+    }
+}
