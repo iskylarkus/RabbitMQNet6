dc69151da09f93454a4bb77b247ae3b40479e187
iskylarkus <turgay.kibar@gmail.com>
2022-04-22 18:36:23 +0300

    product controller and view created

---
+++ RabbitMQNet6.ExcelCreation/Controllers/ProductController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RabbitMQNet6.ExcelCreation.Controllers
+{
+    public class ProductController : Controller
+    {
+        [Authorize]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
