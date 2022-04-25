a281209350d0574876dcc1678b92eff69c479575
iskylarkus <turgay.kibar@gmail.com>
2022-04-25 18:50:32 +0300

    product entity created

---
+++ RabbitMQNet6.FileCreationWorkerService/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RabbitMQNet6.FileCreationWorkerService.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+
+        [Range(0, 900)]
+        public int Stock { get; set; }
+    }
+}
