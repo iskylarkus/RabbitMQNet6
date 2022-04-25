c0d60e017c76561d7e3b252727da7267b043139f
iskylarkus <turgay.kibar@gmail.com>
2022-04-25 16:59:05 +0300

    product create excel and files actions developed

--- RabbitMQNet6.ExcelCreation/Controllers/ProductController.cs
+++ RabbitMQNet6.ExcelCreation/Controllers/ProductController.cs
@@ -1,14 +1,55 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RabbitMQNet6.ExcelCreation.Models;
 
 namespace RabbitMQNet6.ExcelCreation.Controllers
 {
     public class ProductController : Controller
     {
+        private readonly AppDbContext _appDbContext;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public ProductController(AppDbContext appDbContext, UserManager<IdentityUser> userManager)
+        {
+            _appDbContext = appDbContext;
+            _userManager = userManager;
+        }
+
         [Authorize]
         public IActionResult Index()
         {
             return View();
+        }
+
+        public async Task<IActionResult> CreateProductExcel()
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+
+            var fileName = $"product-excel-{Guid.NewGuid().ToString()}";
+
+            UserFile userFile = new()
+            {
+                UserId = user.Id,
+                FileName = fileName,
+                FileStatus = FileStatus.Creating
+            };
+
+            await _appDbContext.UserFiles.AddAsync(userFile);
+
+            await _appDbContext.SaveChangesAsync();
+
+            TempData["StartCreatingExcel"] = true;
+
+            return RedirectToAction(nameof(Files));
+        }
+
+        public async Task<IActionResult> Files()
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+
+            return View(await _appDbContext.UserFiles.Where(x => x.UserId == user.Id).ToListAsync());
         }
     }
 }
