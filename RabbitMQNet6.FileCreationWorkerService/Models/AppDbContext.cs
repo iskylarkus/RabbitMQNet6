465dbd11697029ecdc8094a93f1f36c7ec8b4d73
iskylarkus <turgay.kibar@gmail.com>
2022-04-25 18:51:04 +0300

    dbcontext and sql server connection implemented

---
+++ RabbitMQNet6.FileCreationWorkerService/Models/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RabbitMQNet6.FileCreationWorkerService.Models
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
--- RabbitMQNet6.FileCreationWorkerService/Program.cs
+++ RabbitMQNet6.FileCreationWorkerService/Program.cs
@@ -1,5 +1,7 @@
+using Microsoft.EntityFrameworkCore;
 using RabbitMQ.Client;
 using RabbitMQNet6.FileCreationWorkerService;
+using RabbitMQNet6.FileCreationWorkerService.Models;
 using RabbitMQNet6.FileCreationWorkerService.Services;
 
 IHost host = Host.CreateDefaultBuilder(args)
@@ -13,8 +15,30 @@ IHost host = Host.CreateDefaultBuilder(args)
             DispatchConsumersAsync = true
         });
 
+        services.AddDbContext<AppDbContext>(options =>
+        {
+            options.UseSqlServer(hostContext.Configuration.GetConnectionString("SqlServer"));
+        });
+
         services.AddSingleton<RabbitMQClientService>();
     })
     .Build();
+
+using (var scope = host.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+    context.Database.Migrate();
+
+    if (!context.Products.Any())
+    {
+        Enumerable.Range(1, 30).ToList().ForEach(x =>
+        {
+            context.Products.Add(new Product() { Name = $"Kalem {x}", Price = x * 19, Stock = x * 11 });
+        });
+
+        context.SaveChanges();
+    }
+}
 
 await host.RunAsync();
