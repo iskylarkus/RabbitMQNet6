98caf81d07eec9772c285a623fb890c2e5b79945
iskylarkus <turgay.kibar@gmail.com>
2022-04-26 18:30:44 +0300

    file controller developed for uploading

---
+++ RabbitMQNet6.ExcelCreation/Controllers/FilesController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RabbitMQNet6.ExcelCreation.Models;
+
+namespace RabbitMQNet6.ExcelCreation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public FilesController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Upload(IFormFile file, int fileId)
+        {
+            if (file is not { Length: > 0})
+            {
+                return BadRequest();
+            }
+
+
+            var userFile = await _appDbContext.UserFiles.FirstAsync(x => x.Id == fileId);
+
+            var filePath = userFile?.FileName + Path.GetExtension(file.FileName);
+
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/files/", filePath);
+
+
+            using FileStream fileStream = new(path, FileMode.Create);
+
+            await file.CopyToAsync(fileStream);
+
+
+            userFile.CreatedDate = DateTime.Now;
+
+            userFile.FilePath = filePath;
+
+            userFile.FileStatus = FileStatus.Completed;
+
+            await _appDbContext.SaveChangesAsync();
+
+
+            return Ok();
+        }
+    }
+}
